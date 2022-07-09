c2116e13be1a660609d5ee68fa5fe0e784a87815
Özgür Can Altınok <ozgurcanaltinok@gmail.com>
2022-07-09 20:03:30 +0300

    Use variable in useMousePosition

--- app/src/hooks/useMousePosition.jsx
+++ app/src/hooks/useMousePosition.jsx
@@ -1,19 +1,26 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
+import CSSVars from '../constants'
 
 const useMousePosition = () => {
-  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [position, setPosition] = useState({ x: 0, y: 0 })
 
   useEffect(() => {
-    // eslint-disable-next-line max-len
-    const setMousePosition = (e) => setPosition({ x: (e.clientX) / (window.innerWidth / (500 - 80)), y: e.clientY });
-    window.addEventListener('mousemove', setMousePosition);
+    const setMousePosition = (e) => {
+      setPosition(
+        {
+          x: (e.clientX) / (window.innerWidth / (CSSVars.containerWidth - CSSVars.paddleWidth)),
+          y: e.clientY,
+        },
+      )
+    }
+    window.addEventListener('mousemove', setMousePosition)
 
     return () => {
-      window.removeEventListener('mousemove', setMousePosition);
-    };
-  }, []);
+      window.removeEventListener('mousemove', setMousePosition)
+    }
+  }, [])
 
   return position
-};
+}
 
 export default useMousePosition
