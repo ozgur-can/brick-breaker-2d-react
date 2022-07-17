5037f75c263366e51fafe665f4af51c6fa227de7
Özgür Can Altınok <ozgurcanaltinok@gmail.com>
2022-07-18 01:40:26 +0300

    remove unused parts

--- app/src/store/reducer.js
+++ app/src/store/reducer.js
@@ -1,22 +1,14 @@
-import { Circle, System } from 'detect-collisions'
-// import CollisionDetection from '../models/CollisionDetection'
 import GameObject from '../models/GameObject'
 
 const initialState = {
   refs: [],
   gameObjects: [],
   gameObjectsCount: 0,
-  system: new System(),
 }
 
 const reducer = (state, action) => {
   switch (action.type) {
     case 'INIT_GAME_OBJECT': {
-      console.log(action)
-      if (action.objectType === 'ball') {
-        // const circle = new Circle({ x: 10, y: 100 }, 10)
-      }
-      // state.system.insert()
       return {
         ...state,
         // refs: state.refs.concat(action.ref),
